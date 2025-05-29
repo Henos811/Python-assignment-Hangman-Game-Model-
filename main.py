97f4574760203ace9b66175bd96deecca061a7c4
Henos Tadesse <henos811@gmail.com>
2025-05-29 19:59:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pygame
-import os
 
 pygame.init()
-WIDTH = 800
-HEIGHT = 500
+WIDTH = 1000
+HEIGHT = 700
 pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Hangman Game Model')
 
